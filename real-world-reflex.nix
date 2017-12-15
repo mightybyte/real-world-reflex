4e1998f7d07273c35707f8c733f3b837732db02a
Shea Levy <shea@shealevy.com>
2017-12-15 07:58:56 -0500

    Simplify nix build.

--- real-world-reflex.nix
+++ real-world-reflex.nix
@@ -2,7 +2,14 @@
 
 stdenv.mkDerivation {
   name = "real-world-reflex-0.1";
-  builder = ./builder-nix.sh;
   src = ./.;
-  inherit pandoc;
+  buildInputs = [ pandoc ];
+  buildPhase = "./build.sh";
+  installPhase =
+    ''
+      mkdir -p $out
+      mv images $out
+      mv reveal.js $out
+      mv index.html style.css $out
+    '';
 }
